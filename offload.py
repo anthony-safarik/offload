22bb25b750498edf54a50dc1a4fb963a831a8fde
Anthony Safarik <anthony.safarik@gmail.com>
2024-05-01 14:07:13 -0700

    added time_it and print modified time

--- offload.py
+++ offload.py
@@ -1,11 +1,28 @@
 from pathlib import Path
 import os
+import time
+import datetime
 
 class FileWalker:
     def __init__(self, source_path):
         self.source_path = Path(source_path)
         self.file_paths = {}
+        self.file_info = {}
+#########################################################################################
 
+    def time_it(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f"Function {func.__name__} took {end_time - start_time} seconds to run.")
+            return result
+        return wrapper
+
+
+
+#########################################################################################
+    @time_it
     def walk_directory(self):
         for root, dirs, files in os.walk(self.source_path):
             for file_name in files:
@@ -16,6 +33,13 @@ class FileWalker:
     def print_file_paths(self):
         for file_path in self.file_paths.values():
             print(f'{file_path.relative_to(self.source_path)} --- {file_path.stat()}')
+
+    @time_it
+    def print_modified_time(self):
+        for file_path in self.file_paths.values():
+            mtime = file_path.stat().st_mtime
+            timestamp_str = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M')
+            print(f'{file_path.relative_to(self.source_path)} --- {timestamp_str}')
 
     def compare_files(self, other):
         return sorted(self.file_paths.keys()) == sorted(other.file_paths.keys())
@@ -38,4 +62,4 @@ if __name__ == "__main__":
     print(walker2.minus(walker))
     print (walker.compare_files(walker2))
 
-    # walker.print_file_paths()
+    walker.print_modified_time()
